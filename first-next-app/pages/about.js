2078a118c2153918a6b5d4f089896b4e49ac9869
김재원 <kw2282222@gmail.com>
2023-02-23 23:00:52 +0900

    next13

--- first-next-app/pages/about.js
+++ first-next-app/pages/about.js
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   /** 파이프라인 연산자 */
   // console.log(Math.random() * 10);
-  Math.random() |> x => x * 10 |> console.log;
+  // Math.random() |> x => x * 10 |> console.log;
 
-  return <div className={styles.container}>안녕</div>;
+  return <div className={styles.container}>about</div>;
 }
